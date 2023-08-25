69269a685b66459c43c574e7ad330a6ebf7eedee
Taures <taurest1988@gmail.com>
2023-08-25 15:16:50 +0300

    выполнил ДЗ 22.1

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from catalog.forms import ProductForm
+from catalog.forms import ProductForm, VersionForm
 from catalog.models import Category, Product
+from catalog.models.version import Version
 
 
 class CategoryListView(ListView):
@@ -46,7 +48,6 @@ class ProductListView(ListView):
         return context_data
 
 
-
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -69,11 +70,30 @@ class ProductUpdateView(UpdateView):
     template_name = 'main/product_form.html'
     success_url = reverse_lazy('catalog:category')
 
+    def get_context_data(self, **kwargs):
+        contex_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+
+        contex_data['formset'] = formset
+        return contex_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+
+        if formset.is_valid:
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
+
 
 class ProductDeleteView(DeleteView):
     model = Product
     form_class = ProductForm
     template_name = 'main/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:category')
-
-
